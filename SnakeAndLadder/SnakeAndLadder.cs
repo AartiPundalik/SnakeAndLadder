5e4d8139699f31e50a93893f2137dd9a4455f745
AartiPundalik <aartipundalik68@gmail.com>
2023-04-08 17:09:59 +0530

    [Aarti] Add : UC1 - Single Player Position

---
+++ SnakeAndLadder/SnakeAndLadder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class SnakeAndLadder
+    {
+        public void SinglePlayer()
+        {
+            int player_no = 1;
+            int start_position = 0;
+            Console.WriteLine("Start Position of Player no {0} is : {1} ", player_no, start_position);
+            Console.ReadLine();
+        }
+    }
+}
+    
+
