c6e47e6c6fc2ac985991a228ae8845816f309031
AartiPundalik <aartipundalik68@gmail.com>
2023-04-08 18:16:17 +0530

    [Aarti] Add : UC7 - For 2 Players

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -10,23 +10,26 @@ namespace SnakeAndLadder
     {
         static void Main(string[] args)
         {
-            SnakeAndLadder snakeandladder = new SnakeAndLadder();
-            snakeandladder.SinglePlayer();
+            //SnakeAndLadder snakeandladder = new SnakeAndLadder();
+            //snakeandladder.SinglePlayer();
 
-            DiceRoll getnumber = new DiceRoll();
-            getnumber.RollTheDice();
+            //DiceRoll getnumber = new DiceRoll();
+            //getnumber.RollTheDice();
 
-            CheckOption No_player = new CheckOption();
-            No_player.Checkforoption();
+            //CheckOption No_player = new CheckOption();
+            //No_player.Checkforoption();
 
-            WinningPosition positionmoves = new WinningPosition();
-            positionmoves.PlayerPosition();
+            //WinningPosition positionmoves = new WinningPosition();
+            //positionmoves.PlayerPosition();
 
-            ExactWinning OBJ = new ExactWinning();
-            OBJ.Position100();
+            //ExactWinning OBJ = new ExactWinning();
+            //OBJ.Position100();
 
-            WinTheGame everyroll = new WinTheGame();
-            everyroll.PositionAfterEveryRoll();
+            //WinTheGame everyroll = new WinTheGame();
+            //everyroll.PositionAfterEveryRoll();
+
+            For2Players for2players = new For2Players();
+            for2players.Twoplayers();
         }
     }
 }
